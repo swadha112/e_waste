868cb89b8874250cbf19032f499a4a0ad3b70fe5
swadha112 <swadha.khatod07@gmail.com>
2025-04-06 14:28:17 +0530

    blockchain integrated successfully

--- lib/chip_tracker_contract.dart
+++ lib/chip_tracker_contract.dart
@@ -9,54 +9,119 @@ class ChipTrackerContract {
   ChipTrackerContract(this.client, this.contractAddress, this.contract);
 
   // Load the contract from the ABI file stored in assets
-  static Future<ChipTrackerContract> load(Web3Client client, EthereumAddress contractAddress) async {
+  static Future<ChipTrackerContract> load(
+      Web3Client client,
+      EthereumAddress contractAddress
+      ) async {
+    // 1. Load the ABI string
     final abiString = await rootBundle.loadString('assets/chip_tracker.abi');
-    final contract = DeployedContract(
+    // 2. Create a DeployedContract instance
+    final deployed = DeployedContract(
       ContractAbi.fromJson(abiString, "ChipTracker"),
       contractAddress,
     );
-    return ChipTrackerContract(client, contractAddress, contract);
+    return ChipTrackerContract(client, contractAddress, deployed);
   }
 
-  // Calls the registerChip function of the contract
-  Future<String> registerChip(String uid, BigInt manufactureDate, Credentials credentials) async {
-    final registerFunction = contract.function("registerChip");
+  // ---------------------------------------------------------------------------
+  // registerChip(string _uid, uint256 _manufactureDate)
+  Future<String> registerChip(
+      String uid,
+      BigInt manufactureDate,
+      Credentials credentials,
+      ) async {
+    final function = contract.function("registerChip");
     final txHash = await client.sendTransaction(
       credentials,
       Transaction.callContract(
         contract: contract,
-        function: registerFunction,
-        parameters: [uid, manufactureDate],
+        function: function,
+        parameters: [
+          uid,
+          manufactureDate,
+        ],
       ),
-      chainId: 11155111, // Adjust this for your network (e.g., Sepolia)
+      chainId: 11155111, // e.g. Sepolia
     );
     return txHash;
   }
 
-  // Calls the recordDisposal function of the contract
-  Future<String> recordDisposal(String uid, BigInt disposalDate, Credentials credentials) async {
-    final disposalFunction = contract.function("recordDisposal");
+  // ---------------------------------------------------------------------------
+  // recordDisposal(string _uid, uint256 _disposalDate, string _disposalLocation)
+  Future<String> recordDisposal(
+      String uid,
+      BigInt disposalDate,
+      String disposalLocation,
+      Credentials credentials,
+      ) async {
+    final function = contract.function("recordDisposal");
     final txHash = await client.sendTransaction(
       credentials,
       Transaction.callContract(
         contract: contract,
-        function: disposalFunction,
-        parameters: [uid, disposalDate],
+        function: function,
+        parameters: [
+          uid,
+          disposalDate,
+          disposalLocation, // MISSING before; now fixed
+        ],
       ),
       chainId: 11155111,
     );
     return txHash;
   }
 
-  // Calls the recordTransferForReuse function of the contract
-  Future<String> recordTransferForReuse(String uid, BigInt transferDate, EthereumAddress newManufacturer, Credentials credentials) async {
-    final transferFunction = contract.function("recordTransferForReuse");
+  // ---------------------------------------------------------------------------
+  // recordDisintegration(string _uid, uint256 _disintegrationDate, string _disintegrationLocation)
+  Future<String> recordDisintegration(
+      String uid,
+      BigInt disintegrationDate,
+      String disintegrationLocation,
+      Credentials credentials,
+      ) async {
+    final function = contract.function("recordDisintegration");
     final txHash = await client.sendTransaction(
       credentials,
       Transaction.callContract(
         contract: contract,
-        function: transferFunction,
-        parameters: [uid, transferDate, newManufacturer],
+        function: function,
+        parameters: [
+          uid,
+          disintegrationDate,
+          disintegrationLocation,
+        ],
+      ),
+      chainId: 11155111,
+    );
+    return txHash;
+  }
+
+  // ---------------------------------------------------------------------------
+  // recordTransferForReuse(
+  //   string _uid,
+  //   uint256 _transferDate,
+  //   address _newManufacturer,
+  //   string _manufacturerLocation
+  // )
+  Future<String> recordTransferForReuse(
+      String uid,
+      BigInt transferDate,
+      EthereumAddress newManufacturer,
+      String manufacturerLocation,
+      Credentials credentials,
+      ) async {
+    final function = contract.function("recordTransferForReuse");
+    final txHash = await client.sendTransaction(
+      credentials,
+      Transaction.callContract(
+        contract: contract,
+        function: function,
+        parameters: [
+          uid,
+          transferDate,
+          newManufacturer,
+          manufacturerLocation, // MISSING before; now fixed
+        ],
       ),
       chainId: 11155111,
     );
